f627d687e43b7319c8ced2e2f6e0e5c13348c88a
itsspriyansh <priyanshkp01@gmail.com>
2024-03-26 13:52:48 +0530

    added controller

---
+++ controllers/jobController.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const Jobs = require("../models/Job");
+
+router.post("/add", async (req, res) => {
+    try {
+        const job = new Jobs(req.body);
+        const savedJob = await job.save();
+        if (!savedJob) {
+            throw new Error("Couldn't add job");
+        }
+        res.status(200).json({ status: true, message: "Job added successfully" });
+    } catch (err) {
+        res.status(400).json({ status: false, message: err.message });
+    }
+})
+
+module.exports = router;
