1b3878aaaec46b06f21b4d770607847a9220adff
itsspriyansh <priyanshkp01@gmail.com>
2024-03-26 15:06:36 +0530

    added cooldown

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -15,4 +15,37 @@ router.post("/add", async (req, res) => {
   }
 });
 
+router.get("/getAll", async (req, res) => {
+  try {
+    const jobs = await Jobs.find();
+    res.status(200).json({ status: true, jobs });
+  } catch (err) {
+    res.status(400).json({ status: false, message: err.message });
+  }
+});
+
+router.patch("/update/:id", async (req, res) => {
+  try {
+    const job = await Jobs.findByIdAndUpdate(req.params.id, req.body);
+    if (!job) {
+      throw new Error("Couldn't update job");
+    }
+    res.status(200).json({ status: true, message: "Job updated successfully" });
+  } catch (err) {
+    res.status(400).json({ status: false, message: err.message });
+  }
+});
+
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const job = await Jobs.findByIdAndDelete(req.params.id);
+    if (!job) {
+      throw new Error("Couldn't delete job");
+    }
+    res.status(200).json({ status: true, message: "Job deleted successfully" });
+  } catch (err) {
+    res.status(400).json({ status: false, message: err.message });
+  }
+});
+
 module.exports = router;
