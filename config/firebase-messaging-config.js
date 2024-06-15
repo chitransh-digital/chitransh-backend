aee613b107d067d25c74704ccf3dace2ec2cdb33
itsspriyansh <priyanshkp01@gmail.com>
2024-06-15 22:54:36 +0530

    enabled messagingConfig

--- config/firebase-messaging-config.js
+++ config/firebase-messaging-config.js
@@ -1,10 +1,10 @@
 const admin = require("firebase-admin");
-// const serviceAccount = require("../community-app-a2ac0-firebase-adminsdk-lqfs8-8cecd3d74d.json");
+const serviceAccount = require("../community-app-a2ac0-firebase-adminsdk-lqfs8-8cecd3d74d.json");
 
 const initializeFirebaseAdmin = () => {
-  // admin.initializeApp({
-  //   credential: admin.credential.cert(serviceAccount),
-  // });
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
 };
 
 module.exports = { initializeFirebaseAdmin };
